775e01106055cce7aca59fa702ad4fb94bc2a528
Echoes <87832930+Kamisat@users.noreply.github.com>
2023-09-07 18:26:57 -0400

    Add files via upload

---
+++ family tree/app.py
@@ -0,0 +1,39 @@
+# importando os módulos da biblioteca flask
+from flask import Flask , render_template
+
+# criando a instância da classe Flask, fornecendo a palavra-chave __name__ como argumento
+app = Flask(__name__)
+
+# escreva as rotas usando funções de decorador
+# rota padrão ou 'URL'
+@app.route("/")
+def home():
+
+    nome = "Gabriel" # escreva seu nome
+    idade = "12" # escreva sua idade
+
+    return render_template('index.html' , nome = nome , idade = idade)
+
+# defina a rota para a página do pai
+@app.route("/dad")
+def dad():
+    return render_template('pai.html' , nome = "Victor" , idade = "35")
+
+# defina a rota para a página da mãe
+@app.route("/mom")
+def mom():
+    return render_template('mae.html' , nome = "Maria" , idade = "37")
+
+# defina a rota para a página do amigo
+@app.route("/friend")
+def friend():
+    return render_template('amigo.html' , nome = "Miguel" , idade = "12")
+
+# adicione outras rotas, se você quiser
+
+
+
+
+# execute o arquivo
+if __name__  ==  '__main__':
+    app.run(debug=True)
